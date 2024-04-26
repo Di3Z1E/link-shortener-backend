dac32e17dd0c3ab5ee8b4a08547f22654f33044f
Di3Z1E <100869868+Di3Z1E@users.noreply.github.com>
2024-04-26 18:49:17 +0300

    Update json_manager.py

--- json_manager.py
+++ json_manager.py
@@ -1,18 +1,38 @@
 import json
 
 class JsonManager:
-    @classmethod
-    def read(self, file_name: str = "links.json"):
-        with open(file_name) as f:
-            data = json.load(f)
-        return data
+    FILE_NAME = "links.json"
 
-    @classmethod
-    def write(self, data, file_name: str = "links.json"):
+    @staticmethod
+    def read() -> list:
+        """
+        Read data from the JSON file.
+
+        Returns:
+            list: List containing JSON data.
+        """
         try:
-            with open(file_name, "w") as f:
+            with open(JsonManager.FILE_NAME) as f:
+                data = json.load(f)
+            return data
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
+
+    @staticmethod
+    def write(data: list) -> bool:
+        """
+        Write data to the JSON file.
+
+        Args:
+            data (list): List containing JSON data to write.
+
+        Returns:
+            bool: True if writing is successful, False otherwise.
+        """
+        try:
+            with open(JsonManager.FILE_NAME, "w") as f:
                 json.dump(data, f)
             return True
         except Exception as e:
+            print("Error writing JSON:", e)
             return False
-
