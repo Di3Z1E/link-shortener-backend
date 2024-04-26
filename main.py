b985d372d32cc12be2a2ae41df6fadd58a6d68b8
Di3Z1E <100869868+Di3Z1E@users.noreply.github.com>
2024-04-26 18:49:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from link_shortener import LinkShortener
 
+def main():
+    """
+    Main function to shorten URLs and display the shortened IDs.
+    """
+    urls = [
+        "https://walla.co.il",
+        "https://nana.co.il"
+    ]
 
-urls = [
-    "https://walla.co.il",
-    "https://nana.co.il"
-]
+    for url in urls:
+        try:
+            shortened_url = LinkShortener.shorten(url)
+            print("Shortened URL:", shortened_url)
+        except Exception as e:
+            print("Error occurred:", e)
 
-for url in urls:
-    id = LinkShortener.shorten(url)
-    print(id)
+if __name__ == "__main__":
+    main()
