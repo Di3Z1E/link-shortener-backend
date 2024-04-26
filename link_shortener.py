03cc974108d7badb8887fc6ac50a880f1b933dfc
Di3Z1E <100869868+Di3Z1E@users.noreply.github.com>
2024-04-26 18:49:37 +0300

    Update link_shortener.py

--- link_shortener.py
+++ link_shortener.py
@@ -2,48 +2,20 @@ import uuid
 from json_manager import JsonManager
 
 class LinkShortener:
-    @classmethod
-    def get_link_by_id(self,  id: str):
-        url_entities = JsonManager.read()
-        for url_entity in url_entities:
-            if url_entity["id"] == id:
-                return url_entity["url"]
-        
-        raise Exception("Your ID is not found")
+    @staticmethod
+    def shorten(url: str) -> str:
+        """
+        Shorten a URL and return its unique ID.
 
-    @classmethod
-    def shorten(self, url: str):
+        Args:
+            url (str): The URL to shorten.
+
+        Returns:
+            str: The unique ID representing the shortened URL.
+        """
         unique_id = str(uuid.uuid4())[:8]
         new_url_entity = {"id": unique_id, "url": url}
         url_entities = JsonManager.read()
         url_entities.append(new_url_entity)
         JsonManager.write(url_entities)
         return unique_id
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
